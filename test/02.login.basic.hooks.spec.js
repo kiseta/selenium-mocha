44cf165e0acbd05ef98bc8c4a905c4a304254a42
Tatyana Kiseleva <kiseta@gmail.com>
2023-07-21 01:57:23 -0700

    added multiple tests

--- test/02.login.basic.hooks.spec.js
+++ test/02.login.basic.hooks.spec.js
@@ -41,3 +41,78 @@ describe("Login page tests - Basic", function() {
     expect(errorMsg).to.contain("Your username is invalid!");
   });
 });
+
+describe("Checkbox tests", function() {
+  this.timeout(50000);
+  let driver;
+
+  beforeEach(async function() {
+    driver = await new Builder().forBrowser("chrome").build();
+  });
+
+  afterEach(async function() {
+    await driver.quit();
+  });
+
+  it("should select checkbox 1 and assert it's selected", async function() {
+    await driver.get("https://the-internet.herokuapp.com/checkboxes");
+    const checkbox1 = await driver.findElement(By.css("input:nth-child(1)"));
+    await checkbox1.click();
+    const isChecked = await checkbox1.isSelected();
+    expect(isChecked).to.be.true;
+  });
+
+  it("should unselect checkbox 2 and assert it's unselected", async function() {
+    await driver.get("https://the-internet.herokuapp.com/checkboxes");
+    const checkbox2 = await driver.findElement(By.css("input:nth-child(3)"));
+    await checkbox2.click();
+    const isChecked = await checkbox2.isSelected();
+    expect(isChecked).to.be.false;
+  });
+});
+
+describe("Dropdown list tests", function() {
+  this.timeout(50000);
+  let driver;
+
+  beforeEach(async function() {
+    driver = await new Builder().forBrowser("chrome").build();
+  });
+
+  afterEach(async function() {
+    await driver.quit();
+  });
+
+  it("should select option 1 from the list and assert it's selected", async function() {
+    await driver.get("https://the-internet.herokuapp.com/dropdown");
+    const dropdown = await driver.findElement(By.id("dropdown"));
+    await driver.findElement(By.css("option[value='1']")).click();
+    const selectedOption = await dropdown.getAttribute("value");
+    expect(selectedOption).to.equal("1");
+  });
+});
+
+describe("JavaScript alerts tests", function() {
+  this.timeout(50000);
+  let driver;
+
+  beforeEach(async function() {
+    driver = await new Builder().forBrowser("chrome").build();
+  });
+
+  afterEach(async function() {
+    await driver.quit();
+  });
+
+  it("Js Alert test", async function() {
+    await driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+    const alertButton = await driver.findElement(By.xpath("//button[contains(text(), 'Click for JS Alert')]"));
+    await alertButton.click();
+
+    const alert = await driver.switchTo().alert();
+    await alert.accept();
+
+    const resultMessage = await driver.findElement(By.id("result")).getText();
+    expect(resultMessage).to.equal("You successfully clicked an alert");
+  });
+});
